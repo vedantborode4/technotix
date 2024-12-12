b6fe03f4f7692f536078590c38d60ba9b8239f0d
Vedant <borodevedant4@gmail.com>
2024-12-13 01:59:02 +0530

    add services component

--- frontend/src/components/cards/servicesCard.jsx
+++ frontend/src/components/cards/servicesCard.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
-function servicesCard() {
+function ServicesCard({service="web development", 
+  sevicedetails="creation of landing page, website, online store for small and medium business",
+  cta="contact us >"
+})
+{
   return (
-    <div>servicesCard</div>
+    <div className="sm:w-96 px-2">
+      <div className='flex flex-col items-start px-1 py-2 my-2 border-black border rounded-xl '>
+        <h3 className='font-semibold  text-xl '>{service}</h3>
+        <p className=''>{sevicedetails}</p>
+        <div className="px-1">
+          <button className='border-black border px-2 pb-1  mt-2 rounded-xl self-center hover:bg-purple-100  hover:font-medium hover:border-2 '
+           onClick={"to be handle"} 
+          >{cta}
+          </button>
+        </div>
+      </div>
+    </div>
+
   )
 }
 
-export default servicesCard
+export default ServicesCard
--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import ServicesCard from '../components/cards/servicesCard.jsx'
 
 function Services() {
   return (
-    <div>Services</div>
+    <div className="py-2 ">
+      <div className="text-center text-2xl font-bold p-4 ">
+        <h3 >How We Can Help You</h3>
+      </div>
+      <div className="">
+        <div className=" w-screen flex justify-center pb-7">
+          <div className=' grid grid-cols-1 md:grid-cols-2 gap-4'>
+            <ServicesCard 
+              service="Custom Website Design"  
+              sevicedetails="Create visually stunning, user-friendly websites that reflect your brand and engage your audience."
+            />
+            <ServicesCard 
+              service="Website Development"  
+              sevicedetails="Creation of landing pages, website and online stores for small and medium size business."
+            />
+            <ServicesCard 
+              service="Responsive Design"  
+              sevicedetails="Ensure your website looks and performs on all devices, from desktop to smartphones"
+            />
+            <ServicesCard 
+              service="UI/UX Design"  
+              sevicedetails="User interface development for website and application, design system engineering."
+            />
+            <ServicesCard 
+              service="SEO Optimization"  
+              sevicedetails="Boost your visibility on search engines with our SEO-friendly design and development practices."
+            />
+            <ServicesCard 
+              service="Ongoing Maintenance & Support"  
+              sevicedetails="We provide reliable maintenance and updates to keep your website running smoothly."
+            />
+          </div>
+        </div>    
+      </div>
+    </div>
   )
 }
 
