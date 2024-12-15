0aafb3824de44ce5e0357a4a6705784deabf40a1
Vedant <borodevedant4@gmail.com>
2024-12-15 14:14:37 +0530

    add Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import  { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import Btn from "./btn";
+import Logo from "./Logo";
 
-function Navbar() {
+const Navbar = () => {
+  const Links = [
+    { name: "Home", link: "/" },
+    { name: "About", link: "/about" },
+    { name: "Service", link: "/service" },
+    { name: "Contact", link: "/contact" },
+  ];
+  const [open, setOpen] = useState(false);
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      {/* <div className="overflow-x-hidden shadow-xl w-full fixed top-0 left-0 z-50" > */}
+      <div className="shadow-md w-full fixed top-0 left-0 z-50">
+        <div className="md:flex items-center justify-between bg-white py-2 md:px-10 px-7">
+          <div className="font-bold text-2xl cursor-pointer flex items-center text-gray-800">
+            <span className="text-3xl text-indigo-600 mr-1 ">
+              <Logo />
+            </span>
+          </div>
+          <div
+            onClick={() => setOpen(!open)}
+            className="text-3xl absolute right-8 top-3 cursor-pointer md:hidden"
+          >
+            <GiHamburgerMenu className={open ? "close" : "menu"} />
+          </div>
+          <ul className={`md:flex md:items-center md:pb-0 pb-2 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+            open ? "top-12 " : "top-[-490px]"
+          }`}>
+            {Links.map((link) => (
+              <li key={link.name} className="md:ml-8 text-base font:mediun md:my-0 my-7">
+                <a href={link.link} className="text-gray-800 hover:text-gray-400 duration-500">
+                  {link.name}
+                </a>
+              </li>
+            ))}
+            <li className="hidden md:block md:ml-8 text-xl md:my-0 my-7">
+              <Btn/>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
