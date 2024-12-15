2b533ca5e1ccf93e65abf8559202f7a291f23f43
Vedant <borodevedant4@gmail.com>
2024-12-15 16:20:29 +0530

    add Logo img and component

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import logo from "../assets/img/Logo1.png"
 
 function Logo() {
   return (
-    <div>Logo</div>
+    <div >
+      <img src={logo} alt="Technotix logo" className="h-8 w-15 md:h-10  rounded-lg mt-1"/>
+    </div>
   )
 }
 
