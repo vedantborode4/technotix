9a38b1b0b8b292b84eb4d9544dec5d0ad913458b
Vedant <borodevedant4@gmail.com>
2024-12-15 16:22:35 +0530

    add pages to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Services from './pages/Services.jsx'
+import WhyUs from './pages/WhyUs.jsx'
+import Testimonials from './pages/Testimonials.jsx'
+import ContactUs from './pages/ContactUs.jsx'
+import About from './pages/About.jsx'
+import Home from './pages/Home.jsx'
+import Footer from './components/Footer.jsx'
+
 
 function App() {
   return (
-    <div>App</div>
+    <>
+    <Home />
+    <About/>
+    <Services />
+    <WhyUs />
+    <Testimonials />
+    <ContactUs />
+    <Footer/>
+    </>
   )
 }
 
